d9d7403780c79be0d4bd83b707584a5d619be9e8
yashumak <patil.rtx@gmail.com>
2025-09-07 20:48:06 +0530

    Remove the Unused component and its contents for future reference.

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,21 +1,21 @@
-'use client'
+// 'use client'
 
-import * as React from 'react'
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+// import * as React from 'react'
+// import {
+//   ThemeProvider as NextThemesProvider,
+//   type ThemeProviderProps,
+// } from 'next-themes'
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return (
-    <NextThemesProvider
-      {...props}
-      forcedTheme={undefined}
-      enableSystem={true}
-      attribute="class"
-      defaultTheme="light"
-    >
-      {children}
-    </NextThemesProvider>
-  )
-}
+// export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+//   return (
+//     <NextThemesProvider
+//       {...props}
+//       forcedTheme={undefined}
+//       enableSystem={true}
+//       attribute="class"
+//       defaultTheme="light"
+//     >
+//       {children}
+//     </NextThemesProvider>
+//   )
+// }
